aeb04891fcde16a72be9a9240995bd2653c8f18d
WerpNSK <mininnick@yandex.ru>
2024-09-16 12:27:21 +0700

    (dev) Develop Task-3

--- src/main/java/app/config/AppConfig.java
+++ src/main/java/app/config/AppConfig.java
@@ -1,9 +1,28 @@
 package app.config;
 
+import app.model.Animal;
+import app.model.Cat;
+import app.model.Dog;
+import app.model.Timer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ComponentScan(basePackages = "app")
 public class AppConfig {
+    @Bean(name = "cat")
+    public Animal cat(){
+        return new Cat();
+    }
+
+    @Bean(name = "dog")
+    public Animal dog(){
+        return new Dog();
+    }
+
+    @Bean
+    public Timer timer(){
+        return new Timer();
+    }
 }
